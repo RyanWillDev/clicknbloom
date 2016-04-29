ede750da2efb1d069941635b4ae881009cac75e4
Ryan  Will <ryanwilldev@gmail.com>
2016-04-29 16:25:26 -0500

    added compress gulp tasks added a dist folder added dist to gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-// var bourbon = require('node-bourbon'); // Testing if these lines are required
-// var neat = require('node-neat');
+var uglify = require('gulp-uglify');
+var cleanCss = require('gulp-clean-css');
 
 // Compiles Sass
-
 gulp.task('compileSass', function() {
   return gulp.src('assets/sass/main.scss')
   .pipe(sourcemaps.init())
@@ -20,3 +19,19 @@ gulp.task('compileSass', function() {
 gulp.task('watchSass', function() {
   gulp.watch('assets/sass/**/*.scss', ['compileSass']);
 });
+
+// Minify CSS
+gulp.task('compressCSS', function() {
+  return gulp.src('assets/main.css')
+  .pipe(cleanCss())
+  .pipe(gulp.dest('dist/assets/'));
+});
+
+gulp.task('compressJS', function() {
+  return gulp.src('assets/js/main.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/assets/js'));
+});
+
+// Compress Assets
+gulp.task('compress', ['compressCSS', 'compressJS']);
